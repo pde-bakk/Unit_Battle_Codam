8d2f6c75acc3d31708583fdd09d957831e6572c5
pde-bakk <pde-bakk@student.codam.nl>
2022-06-01 16:37:42 +0200

    first step is to write failing tests, and tahts what we did

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         ::::::::             #
-#    Makefile                                           :+:      :+:    :+:    #
+#    Makefile                                           :+:    :+:             #
 #                                                      +:+                     #
 #    By: saladuit <safoh@student.codam.nl>            +#+                      #
 #                                                    +#+                       #
 #    Created: 2022/04/13 21:33:38 by saladuit      #+#    #+#                  #
-#    Updated: 2022/05/30 23:50:23 by safoh            ###   ########.fr        #
+#    Updated: 2022/06/01 16:30:30 by khuang        ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,7 +17,7 @@ PROJECT				:=	Unit-Battle
 NAME				:=	bowling_game
 
 MAIN 				:=	main.c
-SRCS				=
+SRCS				=	bowling.c
 UNIT_SRCS			=	unit_test.c
 
 
@@ -36,7 +36,7 @@ OBJS				=	$(addprefix $(BUILD_DIR), $(SRCS:%.c=%.o))
 
 UNIT_DIR			:=	unit_test/$(HEADERS_DIR)
 UNIT_HEADERS		:=	$(UNIT_DIR)unit_test.h
-HEADERS				:=	$(HEADERS_DIR)bowling_game.h
+HEADERS				:=	$(HEADERS_DIR)bowling.h
 INCLUDE_FLAGS		:= $(addprefix -I, $(sort $(dir $(HEADERS))))
 
 
@@ -76,11 +76,11 @@ fsan:
 
 clean:
 	$(RM) $(OBJS) $(MAIN_OBJ) $(UNIT_TEST_OBJS) $(COVERAGE)
-	@$(MAKE) clean -C $(LIB_DIR)
+	# @$(MAKE) clean -C $(LIB_DIR)
 
 fclean: clean
 	$(RM) $(NAME) $(UNIT_TEST)
-	@$(MAKE) fclean -C $(LIB_DIR)
+	# @$(MAKE) fclean -C $(LIB_DIR)
 
 re: fclean
 	$(MAKE)
--- unit_test/src/unit_test.c
+++ unit_test/src/unit_test.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   unit_test.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: safoh <safoh@student.codam.nl>             +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/26 16:46:15 by safoh             #+#    #+#             */
-/*   Updated: 2022/05/30 23:43:09 by safoh            ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   unit_test.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: safoh <safoh@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/05/26 16:46:15 by safoh         #+#    #+#                 */
+/*   Updated: 2022/06/01 16:29:47 by khuang        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,7 @@ double ft_pow(double x, double y)
 {
 	return (x * y);
 }
-Test(Example_Tests, should_pass_all_tests_provided)
-{
-	tester(1.0, 1.0, 1.0);
-}
+
 
 void	tester(double x, double y, double expected)
 {
@@ -30,4 +27,18 @@ void	tester(double x, double y, double expected)
 		"Submitted:\t%f for %f to the power of %f\nExpected:\t%f\n",
 		submitted, x, y, expected
 		);
+}
+
+// returns score
+int	get_next_score(int *input[], int *index, int extra);
+int collect_next_roundscore(int *input[], int *index);
+
+Test(Example_Tests, should_pass_all_tests_provided)
+{
+	tester(1.0, 1.0, 1.0);
+	const int input[] = {9, 1, 8, 1, 8, 1, 8, 1, 8, 1, 8, 1, 8, 1, 8, 1, 8, 1, 8, 1};
+	int index = 0;
+	int first_roundscore = collect_next_roundscore((int**)&input, &index);
+	cr_assert( first_roundscore == 18, "Submitted: %d, expected %d\n", first_roundscore, 18);
+	cr_assert( index == 2, "Index now is %d, expected %d\n", index, 2);
 }
